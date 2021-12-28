50c17cc0f67ea4635b87b437a50fec3ee2066bb1
olivianye1 <onye@tulane.edu>
2021-12-28 17:21:38 -0600

    DAY2 part 2

--- src/main/java/Day2/Day2.java
+++ src/main/java/Day2/Day2.java
@@ -12,10 +12,11 @@ public class Day2 {
 
     private InputParser inputParser = new InputParser();
 
+    // Part 1
     public int calculateFinalPosition(String plannedCourseFilePath) throws FileNotFoundException {
         List<String> courseList = inputParser.parseInputForStringList(plannedCourseFilePath);
 
-        if (courseList.size() > 1) {
+        if (courseList.size() > 0) {
             AtomicInteger horizontalPosition = new AtomicInteger();
             AtomicInteger depth = new AtomicInteger();
 
@@ -30,6 +31,37 @@ public class Day2 {
                     depth.addAndGet(steps);
                 } else {
                     depth.addAndGet(-steps);
+                }
+            });
+            return depth.get() * horizontalPosition.get();
+        } else {
+            return 0;
+        }
+    }
+
+    // Part 2
+    public int calculateFinalPositionWithAim(String plannedCourseFilePath) throws FileNotFoundException {
+        List<String> courseList = inputParser.parseInputForStringList(plannedCourseFilePath);
+
+        if (courseList.size() > 0) {
+            AtomicInteger horizontalPosition = new AtomicInteger();
+            AtomicInteger depth = new AtomicInteger();
+            AtomicInteger aim = new AtomicInteger();
+
+            courseList.forEach(c -> {
+                int spacerIndex = c.indexOf(' ');
+                String direction = c.substring(0, spacerIndex);
+                int steps = parseInt(c.substring(spacerIndex + 1));
+
+                if ("forward".equals(direction)) {
+                    horizontalPosition.addAndGet(steps);
+                    if (aim.get() != 0) {
+                        depth.addAndGet(steps * aim.get());
+                    }
+                } else if ("down".equals(direction)) {
+                    aim.addAndGet(steps);
+                } else {
+                    aim.addAndGet(-steps);
                 }
             });
             return depth.get() * horizontalPosition.get();
--- src/test/java/Day2Test.java
+++ src/test/java/Day2Test.java
@@ -8,19 +8,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day2Test {
     private Day2 day2 = new Day2();
 
+    // Part 1 Tests
     @Test
     void calculateFinalPositionTestData() throws FileNotFoundException {
-        int expectedHorizontalPosition = 15;
-        int expectedDepth = 12;
+        int expectedPosition = 150;
 
         assertThat(day2.calculateFinalPosition("/Users/olivianye/Projects/AOC_2021/src/main/java/Day2/Day2TestInput"))
-                .isEqualTo(expectedHorizontalPosition * expectedDepth);
+                .isEqualTo(expectedPosition);
     }
 
     @Test
     void calculateFinalPositionAOCData() throws FileNotFoundException {
         int expectedPosition = 1727835;
         assertThat(day2.calculateFinalPosition("/Users/olivianye/Projects/AOC_2021/src/main/java/Day2/Day2AocInput"))
+                .isEqualTo(expectedPosition);
+    }
+
+    // Part 2 Tests
+    @Test
+    void calculateFinalPositionWithAimTestData() throws FileNotFoundException {
+        int expectedPosition = 900;
+
+        assertThat(day2.calculateFinalPositionWithAim("/Users/olivianye/Projects/AOC_2021/src/main/java/Day2/Day2TestInput"))
+                .isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void calculateFinalPositionWithAimAocData() throws FileNotFoundException {
+        int expectedPosition = 1544000595;
+        assertThat(day2.calculateFinalPositionWithAim("/Users/olivianye/Projects/AOC_2021/src/main/java/Day2/Day2AocInput"))
                 .isEqualTo(expectedPosition);
     }
 }
