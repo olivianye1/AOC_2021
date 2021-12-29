f6d5e11b8bc1424cd29bc08d4905085a68ad13e9
olivianye1 <onye@tulane.edu>
2021-12-29 12:04:17 -0600

    DAY3 part 2

--- src/main/java/Day3/Day3.java
+++ src/main/java/Day3/Day3.java
@@ -4,12 +4,14 @@ import Utils.InputParser;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
 public class Day3 {
     InputParser inputParser = new InputParser();
 
+    // Part 1
     public int getPowerConsumption(String diagnosticReportFilePath) throws FileNotFoundException {
         List<String> binaries = inputParser.parseInputForStringList(diagnosticReportFilePath);
 
@@ -38,8 +40,59 @@ public class Day3 {
         }
     }
 
-    private String toggleBinary (String binary) {
+    private String toggleBinary(String binary) {
         return binary.replaceAll("0", "x").replaceAll("1", "0").replaceAll("x", "1");
+    }
+
+    // Part 2
+    public int getLifeSupportRating(String diagnosticReportFilePath) throws FileNotFoundException {
+        List<String> binaries = inputParser.parseInputForStringList(diagnosticReportFilePath);
+        return getOxygenGeneratorRating(binaries) * getCo2ScrubberRating(binaries);
+    }
+
+    public int getOxygenGeneratorRating(List<String> binaries) {
+
+        if (binaries.size() == 0) {
+            return 0;
+        } else if (binaries.size() == 1){
+            return parseInt(binaries.get(0), 2);
+        } else {
+            int bitsAnalyzed = 0;
+            while (binaries.size() > 1) {
+                int majorityThreshold = (int) Math.ceil(((double) binaries.size() / 2));
+                int bitOfInterest = bitsAnalyzed;
+                int oneCount = (int) binaries.stream().filter(b -> b.charAt(bitOfInterest) == '1').count();
+                if (oneCount >= majorityThreshold) {
+                    binaries = binaries.stream().filter(b -> b.charAt(bitOfInterest) == '1').collect(Collectors.toList());
+                } else {
+                    binaries = binaries.stream().filter(b -> b.charAt(bitOfInterest) == '0').collect(Collectors.toList());
+                }
+                bitsAnalyzed += 1;
+            }
+            return parseInt(binaries.get(0), 2);
+        }
+    }
+
+    public int getCo2ScrubberRating(List<String> binaries) {
+        if (binaries.size() == 0) {
+            return 0;
+        } else if (binaries.size() == 1){
+            return parseInt(binaries.get(0), 2);
+        } else {
+            int bitsAnalyzed = 0;
+            while (binaries.size() > 1) {
+                int minorityThreshold = (int) Math.floor(((double) binaries.size() / 2));
+                int bitOfInterest = bitsAnalyzed;
+                int zeroCount = (int) binaries.stream().filter(b -> b.charAt(bitOfInterest) == '0').count();
+                if (zeroCount <= minorityThreshold) {
+                    binaries = binaries.stream().filter(b -> b.charAt(bitOfInterest) == '0').collect(Collectors.toList());
+                } else {
+                    binaries = binaries.stream().filter(b -> b.charAt(bitOfInterest) == '1').collect(Collectors.toList());
+                }
+                bitsAnalyzed += 1;
+            }
+            return parseInt(binaries.get(0), 2);
+        }
     }
 }
 
